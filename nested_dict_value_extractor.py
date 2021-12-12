63eca5fe2f29295a365963df4fd7cff15bbed7ac
Karthikeyan Janakiraman <karthikeyan.jany@gmail.com>
2021-12-12 18:04:28 +0000

    added docstring

--- nested_dict_value_extractor.py
+++ nested_dict_value_extractor.py
@@ -2,6 +2,13 @@ sample_dict = {"a": {"b": {"c": "d"}}}
 
 
 def value_extractor(given_key, given_dict):
+    """
+    Extracts value from a dictionary for a given key
+
+    :param given_key: key from a nested dictionary
+    :param given_dict: python dictionary
+    :return: value of the key
+    """
     global result
     for k, v in given_dict.items():
         if k != given_key:
